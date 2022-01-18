68211fd3ca01ad59838b9be3f15da8b6b5c7069b
Krisnadjie <krisnadjienatawijaya.2019@student.uny.ac.id>
2022-01-18 18:15:58 +0700

    coba

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <form action="">    
+
+        <div class="links">
+            <h3 class="title">sign in with </h3>
+            <div class="buttons">
+                <a haref="#" class="btn"> <i class="fab fa=google"></i> google </a>
+                <a haref="#" class="btn"> <i class="fab fa=google"></i> facebook </a>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+</body>
+</html>
